0df29e4c3a7e44e8f05ed92cec43058e15806233
RodceVedka <rodce.4@gmail.com>
2022-06-29 19:13:47 -0600

    iitial commit

--- Lesso4/leazy.js
+++ Lesso4/leazy.js
@@ -2,7 +2,7 @@
 const imagesToLoad = document.querySelectorAll("img[data-src]");
 
 //optional parameters being set for the ItersectionalObserver
-const ImgOption = { threshold: 0, rootMargin: "0px 0px 50px 0px"};
+const ImgOption = { threshold: 5, rootMargin: "0px 0px 50px 0px"};
 
 const loadImages = (image) =>{ image.setAttribute("src", image.getAttribute("data-src"));
                                 image.onload = () => {image.removeAttribute("data-src");};};
