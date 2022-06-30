b1e02a0864b99c80e3b5be9744954c5d7202879e
RodceVedka <rodce.4@gmail.com>
2022-06-29 18:19:48 -0600

    iitial commit

--- Lesso4/leazy.js
+++ Lesso4/leazy.js
@@ -1,54 +1,27 @@
+// Get all imgs with data-src attribute 
+const imagesToLoad = document.querySelectorAll("img[data-src]");
 
-/* provide information about */
-let imagesToLoad = document.querySelectorAll('img[data-src]');
-let srcToLoad = document.querySelectorAll('source[data-src]');
-let options = {threshold: .25};
+//optional parameters being set for the ItersectionalObserver
+const ImgOption = { threshold: 0, rootMargin: "0px 0px 50px 0px"};
 
+const loadImages = (image) =>{ image.setAttribute("src", image.getAttribute("data-src"));
+                                image.onload = () => {image.removeAttribute("data-src");};};
 
-const loadImages = (image) => {
-    image.setAttribute('src', image.getAttribute('data-src'));
-    image.onload = () => {
-        image.removeAttribute('data-src');
-    };
-};
+// first check to see if the intersection is supported
+if("IntersectionOserver" in window) {
+   const imgObserver =  new IntersectionObserver((items, observer) =>{items.forEach((item) =>{if(item.isIntersecting) {
+    loadSrcSet(item.target);
+    srcObserver.unobserve(item.target);}});}, ImgOption);
 
-const loadSrcSet = (srcset) => {
-    srcset.setAttribute('srcset', srcset.getAttribute('data-src'));
-    srcset.removeAttribute('data-src');
-};
+   //loop through each img on check status and load if necessary
+   imagesToLoad.forEach(
+    (img) => { imgObserver.observe(img);});
 
-if('IntersectionObserver' in window) {
-    // create an instance of the Observer class called observer
-    const imgObserver = new IntersectionObserver((imagesToLoad, observer) => {
-        imagesToLoad.forEach((item) => {
-        if(item.isIntersecting) {
-            loadImages(item.target);
-            imgObserver.unobserve(item.target);
-        }
-        });
-    }, {threshold: 0.5});
+}
 
-    const srcObserver = new IntersectionObserver((imagesToLoad, observer) => {
-        imagesToLoad.forEach((item) => {
-            if(item.isIntersecting) {
-            loadSrcSet(item.target);
-            srcObserver.unobserve(item.target);
-            }
-        });
-        });
-
-    imagesToLoad.forEach((img) => {
-        imgObserver.observe(img);
-    });
-
-    srcToLoad.forEach((img) => {
-        srcObserver.observe(img);
-    });
-    } else {
-    imagesToLoad.forEach((img) => {
-        loadImages(img);
-    });
-    srcToLoad.forEach((img) => {
-        loadImages(img);
-    });
-    }
+else{  imagesToLoad.forEach((img) => {
+    loadImages(img);
+});
+srcToLoad.forEach((img) => {
+    loadImages(img);
+});}
